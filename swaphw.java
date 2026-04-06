45a877cfeda43d17ec5d3f4c433a5c0d86627037
HP <you@example.com>
2026-04-06 11:46:23 +0530

    heloo

---
+++ swaphw.java
@@ -0,0 +1,20 @@
+class Swap
+{
+	public static void main(String[]args)
+	{
+	  int a=1000;
+	  int b=2000;
+	  int c=3000;
+	  int d=4000;
+	  int temp=a;
+	  a=b;
+	  b=c;
+	  c=d;
+	  d=temp;
+	  System.out.println("A="+a);
+	  System.out.println("B="+b);
+	  System.out.println("C="+c);
+	  System.out.println("D="+d);
+	  
+	}
+}
