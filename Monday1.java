a1624f160b000d886bcd8928c7628b3fe781063b
Mahindhanmahii <146911874+Mahindhanmahii@users.noreply.github.com>
2026-04-06 14:42:56 +0530

    Add files via upload

---
+++ Monday1.java
@@ -0,0 +1,19 @@
+class Monday1
+{ 
+   public static void main (String[] args)
+   { 
+      int a=100;
+	  int b=200;
+	  int c=500;
+	  System.out.println(a+b+c);
+	  System.out.println(a-b-c);
+	  System.out.println(a/b/c);
+	  System.out.println(a%b%c);
+	  System.out.println(a*b*c);
+    }
+
+
+
+
+
+}
